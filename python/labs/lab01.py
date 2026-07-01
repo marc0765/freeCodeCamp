c33bb22d6eb602222590663d1c7ba57e4eaa0766
Maria Rodricks <124487023+marc0765@users.noreply.github.com>
2026-07-01 22:28:29 +0530

    Create Lab 01

---
+++ python/labs/lab01.py
@@ -0,0 +1,15 @@
+# Build a Report Card Printer
+# This program demonstrates different Python data types (string, boolean, integer, and float) and checks their values and types.
+
+name = 'Alice'
+print(name, type(name))
+
+is_student = True
+print(is_student, type(is_student))
+
+age = 20
+print(age, type(age))
+
+score = 80.5
+print(isinstance(score, float))
+print(score, type(score))
